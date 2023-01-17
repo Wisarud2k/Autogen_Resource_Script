bb64cb8e82245dbd2cf00f4056637df556ddbc31
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2023-01-17 15:10:14 +0700

    Nmap module now can export output as a text file

--- app/nmap_module.py
+++ app/nmap_module.py
@@ -1,8 +1,11 @@
 import nmap3
 import json
+import base
+import datetime
 
 def nmap_scan():
     nmap = nmap3.Nmap()
+    date = datetime.datetime.now()
     # results = nmap.scan_top_ports("192.168.184.129")
     # ressults = nmap.nmap_version_detection("host", args="--script vulners --script-args mincvss+5.0")
     # print(results)
@@ -10,6 +13,7 @@ def nmap_scan():
     ip_address = input('Please Input Target IP Address: ')
     try:
         version_result = nmap.nmap_version_detection(ip_address)
+        
     except:
         print('Error: Please try again')
 
@@ -20,21 +24,33 @@ def nmap_scan():
     jj = json.loads(json_formatted_str)
     formated = json.dumps(jj['192.168.184.129']['ports'],indent=4,sort_keys=True)
     data = jj['192.168.184.129']['ports']
-    for port in data:
-        # 4 case 
-        # have all information
-        # print(port)
-        if(port['state'] == 'open' and 'version' in port['service'].keys() and 'product' in port['service'].keys()):
-            print('PORT: ' +port['portid']+'/'+port['protocol'] +' SERVICE: '+port['service']['name'] + ' VERSION: ' + port['service']['product'] + ' ' + port['service']['version'])
-        elif(port['state'] == 'open' and 'version' in port['service'].keys()): # have only version keys
-            # print('Im only have version key!!')
-            print('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name']+ ' VERSION: ' + port['service']['version'])
-        elif(port['state'] == 'open' and 'product' in port['service'].keys()): # have only product keys 
-            # print('Im only have product key!!')
-            print('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name']+ ' VERSION: ' + port['service']['product'])
-        elif(port['state'] == 'open'): # dont have version and product keys
-            # print('Im dont have both extras keys!!')
-            print('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name'] + ' VERSION: ')
+    with open(base.LOCAL_PATH+ ip_address + '_'+ str(date.date()) +'.txt', 'w') as f:
+        f.write("IP: %s\n" % ip_address)
+        print("IP: %s" % ip_address)
+        for port in data:
+            # 4 case 
+            # have all information
+            # print(port)
+            if(port['state'] == 'open' and 'version' in port['service'].keys() and 'product' in port['service'].keys()):
+                print('PORT: ' +port['portid']+'/'+port['protocol'] +' SERVICE: '+port['service']['name'] + ' VERSION: ' + port['service']['product'] + ' ' + port['service']['version'])
+                f.write('PORT: ' +port['portid']+'/'+port['protocol'] +' SERVICE: '+port['service']['name'] + ' VERSION: ' + port['service']['product'] + ' ' + port['service']['version'])
+                f.write('\n')
+            elif(port['state'] == 'open' and 'version' in port['service'].keys()): # have only version keys
+                # print('Im only have version key!!')
+                print('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name']+ ' VERSION: ' + port['service']['version'])
+                f.write('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name']+ ' VERSION: ' + port['service']['version'])
+                f.write('\n')
+            elif(port['state'] == 'open' and 'product' in port['service'].keys()): # have only product keys 
+                # print('Im only have product key!!')
+                print('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name']+ ' VERSION: ' + port['service']['product'])
+                f.write('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name']+ ' VERSION: ' + port['service']['product'])
+                f.write('\n')
+            elif(port['state'] == 'open'): # dont have version and product keys
+                # print('Im dont have both extras keys!!')
+                print('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name'] + ' VERSION: ')
+                f.write('PORT: ' +port['portid']+'/'+port['protocol']+' SERVICE: '+port['service']['name'] + ' VERSION: ')
+                f.write('\n')
+    
 
 def main():
     print('nmap module')
