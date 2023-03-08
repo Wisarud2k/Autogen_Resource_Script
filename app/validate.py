5bab121dca1ccfd15a28fc555537511b35922aa3
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2023-03-08 15:56:30 +0700

    validate script name

--- app/validate.py
+++ app/validate.py
@@ -1,4 +1,6 @@
 import ipaddress
+import re
+
 def validate_ip_address(ip_string):
    try:
        ip_object = ipaddress.ip_address(ip_string)
@@ -11,3 +13,16 @@ def setting_validate(fieldname,value):
         return validate_ip_address(value)
     elif(fieldname == 'TTL'):
         return value.isdigit()
+    
+def is_valid_filename(filename):
+    # Define a regular expression to match valid filenames
+    pattern = r"^[a-zA-Z0-9_-]+\.[a-zA-Z0-9]+$"
+    
+    # Use the regular expression to check whether the filename matches the pattern
+    match = re.match(pattern, filename)
+    
+    # If there's a match, return True; otherwise, return False
+    if match:
+        return True
+    else:
+        return False
