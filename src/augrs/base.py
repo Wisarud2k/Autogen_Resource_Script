dd1db00f50b52642ac0d001d5bba1a29ac77ad50
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2023-03-26 14:52:39 +0700

    remove hard code open port

--- src/augrs/base.py
+++ src/augrs/base.py
@@ -8,6 +8,6 @@ REPORT_PATH = cwd + '/exploit_report/'
 # setting dict for global setting
 settings = {'TTL(s)':'30','LHOSTS':'172.17.0.2'}
 # open port for exploit searching 
-open_port = ['21']
+open_port = []
 exploit_and_port = {}
 exploit_list = []
